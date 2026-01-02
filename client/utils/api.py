064fcb6ebfc2e908980e24e0ba6462434ff1b849
Varun Purohit <varunpurohit3099@gmail.com>
2026-01-02 11:43:38 +0000

    commit

--- client/utils/api.py
+++ client/utils/api.py
@@ -1,10 +1,18 @@
 import requests
 from config import API_URL
 
-
 def upload_pdfs_api(files):
-    files_payload=[ ("files",(f.name,f.read(),"application/pdf")) for f in files]
-    return requests.post(f"{API_URL}/upload_pdfs/",files=files_payload)
+    files_payload = [
+        ("files", (f.name, f.read(), "application/pdf"))
+        for f in files
+    ]
+    return requests.post(
+        f"{API_URL}/upload/upload_pdfs/",
+        files=files_payload
+    )
 
 def ask_question(question):
-    return requests.post(f"{API_URL}/ask/",data={"question":question})
+    return requests.post(
+        f"{API_URL}/ask/ask/",
+        data={"question": question}
+    )
